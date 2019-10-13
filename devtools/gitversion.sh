2b4a01ca202d640f8283bedd5a9b9a2bf4dbfc61
Marcin Smoczyński <smoczynski.marcin@gmail.com>
2019-10-13 23:01:12 +0200

    build: enable building with cmake
    All functions provided by the original Makefile has been preserved
    except for building a coverage report with lcov.
    
    It is not possible to build tests before running them in one step
    because of cmake's limitations.

---
+++ devtools/gitversion.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# check arguments
+if [ $# -ne 1 ]; then
+    exit 1
+fi
+outfile="$1"
+
+topdir="$(realpath $(dirname $0)/..))"
+if [ ! -d "${topdir}/.git" ]; then
+    echo "namespace spyc{const char *gitversion = nullptr;}" > $outfile
+else
+    gitrev="$(git rev-parse --short HEAD)"
+    dirty="$(git diff --quiet || echo '-dirty')"
+    echo "namespace spyc{const char *gitversion = \"${gitrev}${dirty}\";}" > ${outfile}
+fi
